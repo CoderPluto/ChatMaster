5147bf51c949459355f908d4d1e63b0f3249adc8
hkminegod <choyiny@gmail.com>
2013-08-05 22:02:40 +0800

    For Pluto

--- src/main/java/me/techmastary/plugins/silenced/Silenced.java
+++ src/main/java/me/techmastary/plugins/silenced/Silenced.java
@@ -10,7 +10,7 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Silenced extends JavaPlugin implements Listener {
-	public boolean Silence;
+	public static boolean Silence;
 	public void onDisable() {
 	}
 
@@ -18,19 +18,13 @@ public class Silenced extends JavaPlugin implements Listener {
 		getServer().getPluginManager().registerEvents(this, this);
 	}
 	
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		return Silence;
-		
-	}
-	
 	@EventHandler
 	public void OnPlayerJoin(PlayerJoinEvent event) {
-		
 	}
 	
 	@EventHandler
 	public void OnPlayerChat(AsyncPlayerChatEvent event) {
-		if (!event.getPlayer().hasPermission("silenced.admin")) {
+		if (!event.getPlayer().hasPermission("silenced.admin") && (Silence)) {
 			event.setCancelled(true);
 			event.getPlayer().sendMessage(ChatColor.GRAY + "The chat is currently disabled.");
 		}
