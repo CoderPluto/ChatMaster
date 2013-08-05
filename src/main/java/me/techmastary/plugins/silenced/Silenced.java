7b028479aa651c78f24d6b2dd36e3b8864bcd71e
Spencer <spencerp1999@yahoo.com>
2013-08-05 09:52:11 -0500

    Fix the errors hk!

--- src/main/java/me/techmastary/plugins/silenced/Silenced.java
+++ src/main/java/me/techmastary/plugins/silenced/Silenced.java
@@ -1,33 +1,46 @@
-package me.techmastary.plugins.silenced;
-
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Silenced extends JavaPlugin implements Listener {
 	public static boolean Silence;
+	
+	@Override
 	public void onDisable() {
+    	System.out.println("Silencer Disabled");
 	}
-
+	
+    @Override
 	public void onEnable() {
-		getServer().getPluginManager().registerEvents(this, this);
-	}
-	
-	@EventHandler
-	public void OnPlayerJoin(PlayerJoinEvent event) {
-	}
-	
-	@EventHandler
-	public void OnPlayerChat(AsyncPlayerChatEvent event) {
-		if (!event.getPlayer().hasPermission("silenced.admin") && (Silence)) {
-			event.setCancelled(true);
-			event.getPlayer().sendMessage(ChatColor.GRAY + "The chat is currently disabled.");
+    	System.out.println("Silencer Enabled");
+    }
+    
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
+		Player player = (Player) sender;
+		if(commandLabel.equalsIgnoreCase("silence")){
+			if (player.hasPermission("Silence.on")) {
 		}
-	}
+			 onPlayerChat(AsyncPlayerChatEvent event) {
+				    event.setCancelled(true);
+			Bukkit.broadcastMessage(ChatColor.YELLOW + "Chat has been silenced by " + ChatColor.DARK_RED + player.getDisplayName());
 
+		if(commandLabel.equalsIgnoreCase("silence-off")){
+			if (player.hasPermission("Silence.off")) {
+			}	
+
+			Bukkit.broadcastMessage(ChatColor.YELLOW + "Chat is now enabled ");
+			
+			}
+		
+		return true;
+}
+    }
 }
