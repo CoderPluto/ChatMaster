3c9467dea911eaa3f5a8ce63ca8851fe22ac284a
hkminegod <choyiny@gmail.com>
2013-08-05 23:06:29 +0800

    Fixing Pluto

--- src/main/java/me/techmastary/plugins/silenced/Silenced.java
+++ src/main/java/me/techmastary/plugins/silenced/Silenced.java
@@ -1,46 +1,51 @@
-import org.bukkit.Bukkit;
+package me.techmastary.plugins.silenced;
+
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Silenced extends JavaPlugin implements Listener {
 	public static boolean Silence;
-	
+
 	@Override
 	public void onDisable() {
-    	System.out.println("Silencer Disabled");
+		System.out.println("Enabled Silenced.");
 	}
-	
-    @Override
+
+	@Override
 	public void onEnable() {
-    	System.out.println("Silencer Enabled");
-    }
-    
-    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
-		Player player = (Player) sender;
-		if(commandLabel.equalsIgnoreCase("silence")){
-			if (player.hasPermission("Silence.on")) {
-		}
-			 onPlayerChat(AsyncPlayerChatEvent event) {
-				    event.setCancelled(true);
-			Bukkit.broadcastMessage(ChatColor.YELLOW + "Chat has been silenced by " + ChatColor.DARK_RED + player.getDisplayName());
+		System.out.println("Disabled Silenced.");
+	}
 
-		if(commandLabel.equalsIgnoreCase("silence-off")){
-			if (player.hasPermission("Silence.off")) {
-			}	
-
-			Bukkit.broadcastMessage(ChatColor.YELLOW + "Chat is now enabled ");
-			
+	@EventHandler
+	public void OnPlayerChat(AsyncPlayerChatEvent event) {
+		if (!event.getPlayer().hasPermission("silenced.admin")) {
+			if (!event.getPlayer().hasPermission("silenced.admin") && (Silence == true)) {
+				event.setCancelled(true);
+				event.getPlayer().sendMessage(ChatColor.GRAY + "The chat is currently disabled.");
 			}
-		
+		}
+	}
+
+	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+		if (commandLabel.equalsIgnoreCase("silence")) {
+			if (sender.hasPermission("silenced.admin")) {
+				Silence = true;
+			} else {
+				sender.sendMessage(ChatColor.WHITE + "Not cool enough to execute this command.");
+			}
+		}
+		if (commandLabel.equalsIgnoreCase("resume")) {
+			if (sender.hasPermission("silenced.admin")) {
+				Silence = false;
+			} else {
+				sender.sendMessage(ChatColor.WHITE + "Not cool enough to execute this command.");
+			}
+		}
 		return true;
-}
-    }
+	}
 }
