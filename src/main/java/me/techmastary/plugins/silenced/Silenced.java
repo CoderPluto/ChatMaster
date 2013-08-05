b97737b67e37f980f89af7540066d0ad1194b421
hkminegod <choyiny@gmail.com>
2013-08-05 23:22:00 +0800

    Fix Mistake

--- src/main/java/me/techmastary/plugins/silenced/Silenced.java
+++ src/main/java/me/techmastary/plugins/silenced/Silenced.java
@@ -1,5 +1,6 @@
 package me.techmastary.plugins.silenced;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -13,28 +14,29 @@ public class Silenced extends JavaPlugin implements Listener {
 
 	@Override
 	public void onDisable() {
-		System.out.println("Enabled Silenced.");
+		System.out.println("Disabled Silenced.");
+		Silence = false;
 	}
 
 	@Override
 	public void onEnable() {
-		System.out.println("Disabled Silenced.");
+		System.out.println("Enabled Silenced.");
 	}
 
-	@EventHandler
-	public void OnPlayerChat(AsyncPlayerChatEvent event) {
-		if (!event.getPlayer().hasPermission("silenced.admin")) {
-			if (!event.getPlayer().hasPermission("silenced.admin") && (Silence == true)) {
-				event.setCancelled(true);
-				event.getPlayer().sendMessage(ChatColor.GRAY + "The chat is currently disabled.");
+		@EventHandler
+		public void OnPlayerChat(AsyncPlayerChatEvent event) {
+				if (!event.getPlayer().hasPermission("silenced.admin") && (Silence == true)) {
+					event.setCancelled(true);
+					event.getPlayer().sendMessage(ChatColor.GRAY + "The chat is currently disabled.");
+				}
 			}
-		}
-	}
 
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
 		if (commandLabel.equalsIgnoreCase("silence")) {
 			if (sender.hasPermission("silenced.admin")) {
 				Silence = true;
+				sender.sendMessage("You silenced chat.");
+				Bukkit.broadcastMessage(ChatColor.GRAY + "" + sender + "disabled global chat.");
 			} else {
 				sender.sendMessage(ChatColor.WHITE + "Not cool enough to execute this command.");
 			}
